b9031cf17af7c200c5625c162b7d3b74a9088398
J VENKATA LAKSHMI SAI ROHITH <94969985+Rohith2201@users.noreply.github.com>
2024-06-20 20:58:22 +0530

    Create 206. Reverse Linked List.java

---
+++ 206. Reverse Linked List.java
@@ -0,0 +1,17 @@
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+CODE:
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode node = null;
+
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = node;
+            node = head;
+            head = temp;
+        }
+
+        return node;        
+    }
+}
